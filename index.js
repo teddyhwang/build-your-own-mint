59947746f3be1a134c30af451cb2516fd609e653
Teddy Hwang <teddy.hwang@shopify.com>
2020-01-05 10:27:36 -0500

    wip on balances

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ if (process.env['DOT_ENV_PATH']) {
 }
 require('dotenv').config({ path: envPath })
 
-const { fetchTransactions } = require('./lib/fetch')
+const { fetchTransactions, fetchBalances} = require('./lib/fetch')
 const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet } = require('./lib/update')
 
 ;(async () => {
   const transactions = await fetchTransactions()
+  const balances = await fetchBalances()
+  console.log(balances)
   const updates = transformTransactionsToUpdates(transactions)
   updateSheet(updates)
 })()
